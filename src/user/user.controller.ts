b7f04a9ac3daeb2dee57a01ab63e9d5f91a031c1
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-10-27 15:45:34 -0300

    crud

---
+++ src/user/user.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body){
+        
+        return {body};
+    }
+
+    @Get()
+    async read(){
+
+        return {users:[]}
+    }
+
+    @Get(':id')
+    async readOne(@Param() param){
+
+        return {user:{}, param}
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params){
+
+        return {
+            method: 'put',
+            body,
+            params
+        }
+    }
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params){
+
+        return {
+            method: 'patch',
+            body,
+            params
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param() params){
+
+        return { params}
+    }
+
+}
