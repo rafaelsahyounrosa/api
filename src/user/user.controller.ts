c9729c2060ff4b5604e13f84dbc6c85d00faa5e5
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-10-30 08:38:54 -0300

    creating a custom param

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 import { UserService } from "./user.service";
-import { LogInterceptor } from "src/interceptors/log.interceptor";
+import { ParamId } from "src/decorators/param-id.decorator";
 
 //@UseInterceptors(LogInterceptor)
 @Controller('users')
@@ -22,23 +22,24 @@ export class UserController {
     }
 
     @Get(':id')
-    async readOne(@Param('id', ParseIntPipe) id: number){
+    async readOne(@ParamId() id: number){
+        console.log({id});
         return this.userService.readOne(id);
     }
 
     @Put(':id')
-    async update(@Body() data : UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number){
+    async update(@Body() data : UpdatePutUserDTO, @ParamId() id: number){
 
         return this.userService.update(id, data);
     }
     @Patch(':id')
-    async updatePartial(@Body() data: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number){
+    async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number){
 
         return this.userService.updatePartial(id, data);
     }
 
     @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id: number){
+    async delete(@ParamId() id: number){
 
         return this.userService.delete(id);
     }
